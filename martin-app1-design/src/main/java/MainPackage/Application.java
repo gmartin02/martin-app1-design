9575da88e7f08a59a80a61424a692d42c6a312f2
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-23 21:48:28 -0400

    app1-design UML file created

--- martin-app1-design/src/main/java/MainPackage/Application.java
+++ martin-app1-design/src/main/java/MainPackage/Application.java
@@ -26,5 +26,33 @@ public class Application extends javafx.application.Application {
         // create a TipCalculator object and call its start method
         launch(args);
     }
+
+    public ToDoListManager saveToDoLists() {
+        //creates am empty ToDoListManager
+        //adds only the ToDoLists with their selected fields set to true
+        //returns the newly created ToDoListManager to be saved to a file
+        return null;
+    }
+
+    public void writeToFile(ToDoListManager selectedLists) {
+        //writes the manager created in the function above and loops through
+        // its to do lists and each of the items and writes it down in each down on a separate line
+        //for example:
+            //groceries 2
+                //get apple
+                    //granny smith
+                    //2021-10-31
+                //get chicken
+                    //tender
+                    //2021-10-31
+        //the integer after the list name represents the number of items in the list
+    }
+
+    public ToDoListManager loadToDoLists() {
+        //loads the file that contains a ToDoListManager
+        //reads the txt file in the same way described above to populate a new ToDoListManager
+        //returns newly made manager
+        return null;
+    }
 }
 
--- martin-app1-design/src/main/java/MainPackage/ToDoListManager.java
+++ martin-app1-design/src/main/java/MainPackage/ToDoListManager.java
@@ -33,31 +33,5 @@ public class ToDoListManager {
         //changes the title of the list by prompting for a new name and reassigning title
     }
 
-    public ToDoListManager saveToDoLists() {
-        //creates am empty ToDoListManager
-        //adds only the ToDoLists with their selected fields set to true
-        //returns the newly created ToDoListManager to be saved to a file
-        return null;
-    }
 
-    public void writeToFile(ToDoListManager selectedLists) {
-        //writes the manager created in the function above and loops through
-        // its to do lists and each of the items and writes it down in each down on a separate line
-        //for example:
-            //groceries 2
-                //get apple
-                    //granny smith
-                    //2021-10-31
-                //get chicken
-                    //tender
-                    //2021-10-31
-        //the integer after the list name represents the number of items in the list
-    }
-
-    public ToDoListManager loadToDoLists() {
-        //loads the file that contains a ToDoListManager
-        //reads the txt file in the same way described above to populate a new ToDoListManager
-        //returns newly made manager
-        return null;
-    }
 }

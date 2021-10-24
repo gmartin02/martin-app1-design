0669764a5d0f0742316221735d4536959beaa1ec
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-24 19:57:55 -0400

    app1-design test cases added

--- martin-app1-design/src/main/java/MainPackage/Application.java
+++ martin-app1-design/src/main/java/MainPackage/Application.java
@@ -9,13 +9,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Application extends javafx.application.Application {
     @Override
 
     public void start(Stage stage) throws Exception {
 
         Parent root =
-                FXMLLoader.load((getClass().getResource("ToDoListGUI.fxml")));
+                FXMLLoader.load((Objects.requireNonNull(getClass().getResource("ToDoListGUI.fxml"))));
         Scene scene = new Scene(root); // attach scene graph to scene
         stage.setTitle("Tip Calculator"); // displayed in window's title bar
         stage.setScene(scene); // attach scene to stage

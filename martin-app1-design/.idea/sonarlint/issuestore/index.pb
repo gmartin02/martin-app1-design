b9d62c62a122ca6c5059b047224ee6dbed86766e
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-23 20:17:02 -0400

    app1-design iniitial GUI creation

--- martin-app1-design/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-Z
-*src/main/java/MainPackage/Application.java,2\2\227b63a2d43104a07964db4c4431f470d95b98e9
--- martin-app1-design/src/main/java/MainPackage/Application.java
+++ martin-app1-design/src/main/java/MainPackage/Application.java
@@ -1,4 +1,27 @@
 package MainPackage;
 
-public class Application {
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+    @Override
+
+    public void start(Stage stage) throws Exception {
+
+        Parent root =
+                FXMLLoader.load((getClass().getResource("ToDoListGUI.fxml")));
+        Scene scene = new Scene(root); // attach scene graph to scene
+        stage.setTitle("Tip Calculator"); // displayed in window's title bar
+        stage.setScene(scene); // attach scene to stage
+        stage.show(); // display the stage
+    }
+
+    public static void main(String[] args) {
+        // create a TipCalculator object and call its start method
+        launch(args);
+    }
 }
+
